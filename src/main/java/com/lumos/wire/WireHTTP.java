df39c953d60ce3ff13bfb6159d2f91bfa77745eb
leochanj105 <leocjy105@gmail.com>
2023-10-30 14:02:59 -0400

    done remove hardcoded http wiring stuff

--- src/main/java/com/lumos/wire/WireHTTP.java
+++ src/main/java/com/lumos/wire/WireHTTP.java
@@ -20,103 +20,100 @@ import soot.jimple.InvokeExpr;
 import soot.jimple.Stmt;
 
 public class WireHTTP {
-        private static Map<RequestWirePoint, HTTPReceiveWirePoint> wps = null;
+    private static Map<RequestWirePoint, HTTPReceiveWirePoint> wps = null;
 
-        public static HTTPReceiveWirePoint templateWire(Stmt currStmt, MethodInfo minfo) {
-                InvokeExpr expr = currStmt.getInvokeExpr();
-                SootMethod invokedMethod = expr.getMethod();
-                if (Utils.isCrossContext(invokedMethod.toString())) {
-                        String srcLine = Utils.getSourceLine(minfo.sm.getDeclaringClass().getName(),
-                                        +currStmt.getJavaSourceStartLineNumber());
+    public static HTTPReceiveWirePoint templateWire(Stmt currStmt, MethodInfo minfo) {
+        InvokeExpr expr = currStmt.getInvokeExpr();
+        SootMethod invokedMethod = expr.getMethod();
+        if (Utils.isCrossContext(invokedMethod.toString())) {
+            String srcLine = Utils.getSourceLine(minfo.sm.getDeclaringClass().getName(),
+                    +currStmt.getJavaSourceStartLineNumber());
 
-                        for (String address : App.remoteMap.keySet()) {
-                                // App.p(address + ": " + remoteMap.get(address));
-                                if (srcLine.contains(address)) {
-                                        App.p(srcLine);
-                                        SootMethod wiredMethod = App.remoteMap.get(address);
-                                        // p("=========");
-                                        // p(currStmt);
-                                        // p("Matched " + wiredMethod);
-                                        HTTPReceiveWirePoint hwire = new HTTPReceiveWirePoint(
-                                                        wiredMethod.getSubSignature(), new ArrayList<>(),
-                                                        new ArrayList<>());
-                                        if (wiredMethod.getParameterCount() == 1) {
-                                                for (Local plocal : wiredMethod.getActiveBody().getParameterLocals()) {
-                                                        for (Value arg : expr.getArgs()) {
-                                                                // p(arg.getType().toString());
-                                                                // p("++ " + plocal.getType().);
-                                                                // plocal.getType().
-                                                                String callerName = Utils.getDirectName(
-                                                                                arg.getType().toString());
-                                                                String calleeName = Utils.getDirectName(
-                                                                                plocal.getType().toString());
+            for (String address : App.remoteMap.keySet()) {
+                // App.p(address + ": " + remoteMap.get(address));
+                if (srcLine.contains(address)) {
+                    App.p(srcLine);
+                    SootMethod wiredMethod = App.remoteMap.get(address);
+                    // p("=========");
+                    // p(currStmt);
+                    // p("Matched " + wiredMethod);
+                    HTTPReceiveWirePoint hwire = new HTTPReceiveWirePoint(
+                            wiredMethod.getSubSignature(), new ArrayList<>(),
+                            new ArrayList<>());
+                    if (wiredMethod.getParameterCount() == 1) {
+                        for (Local plocal : wiredMethod.getActiveBody().getParameterLocals()) {
+                            for (Value arg : expr.getArgs()) {
+                                // p(arg.getType().toString());
+                                // p("++ " + plocal.getType().);
+                                // plocal.getType().
+                                String callerName = Utils.getDirectName(
+                                        arg.getType().toString());
+                                String calleeName = Utils.getDirectName(
+                                        plocal.getType().toString());
 
-                                                                // if (callerName.contains("Payment")) {
-                                                                // p("++ " + callerName + ", " + calleeName);
-                                                                // }
-                                                                if (Utils.typeMatch(callerName, calleeName)) {
-                                                                        // p("----- " + arg + " matched for remote "
-                                                                        // + plocal);
-                                                                        hwire.getSendParams().add(arg.toString());
-                                                                        hwire.getRecvParams().add(plocal.getName());
-                                                                        if (currStmt instanceof AssignStmt) {
-                                                                                String retName = ((AssignStmt) currStmt)
-                                                                                                .getLeftOp()
-                                                                                                .toString();
-                                                                                App.p("!!!! " + retName + ", " + arg
-                                                                                                + ", " + plocal);
-                                                                                // hwire.setRetWireName(retName);
-                                                                                return hwire;
-                                                                        }
-                                                                }
-                                                        }
-                                                }
-                                        }
-                                        // App.p(expr.getArgs());
+                                // if (callerName.contains("Payment")) {
+                                // p("++ " + callerName + ", " + calleeName);
+                                // }
+                                if (Utils.typeMatch(callerName, calleeName)) {
+                                    // p("----- " + arg + " matched for remote "
+                                    // + plocal);
+                                    hwire.getSendParams().add(arg.toString());
+                                    hwire.getRecvParams().add(plocal.getName());
+                                    if (currStmt instanceof AssignStmt) {
+                                        String retName = ((AssignStmt) currStmt)
+                                                .getLeftOp()
+                                                .toString();
+                                        App.p("!!!! " + retName + ", " + arg
+                                                + ", " + plocal);
+                                        // hwire.setRetWireName(retName);
+                                        return hwire;
+                                    }
                                 }
+                            }
                         }
+                    }
+                    // App.p(expr.getArgs());
                 }
-                return null;
+            }
         }
+        return null;
+    }
 
-        public static HTTPReceiveWirePoint get(String reqMethod, int lineNum) {
+    public static HTTPReceiveWirePoint get(String reqMethod, int lineNum) {
 
-                if (wps == null) {
-                        wps = new HashMap<>();
-                        add("java.util.concurrent.Future sendInsidePayment", 99,
-                                        "inside_payment.service.InsidePaymentServiceImpl: boolean pay(inside_payment.domain.PaymentInfo,javax.servlet.http.HttpServletRequest)",
-                                        new String[] { "$u0", "loginId" }, new String[] { "info", "$stack16" });
-                        add("java.util.concurrent.Future sendOrderCancel",
-                                        115,
-                                        "cancel.controller.CancelController: cancel.domain.CancelOrderResult cancelTicket(cancel.domain.CancelOrderInfo,java.lang.String,java.lang.String)",
-                                        new String[] { "$u0", "loginId", "loginToken" },
-                                        new String[] { "info", "loginId", "loginToken" });
-                        add("cancel.queue.MsgSendingBean: void sendCancelInfoToOrderOther(cancel.domain.ChangeOrderInfo)",
-                                        27, "other.queue.MsgReveiceBean: void receiveQueueInfo(java.lang.Object)",
-                                        new String[] { "changeOrderInfo" },
-                                        new String[] { "changeOrderInfo" });
-                        add("other.queue.MsgSendingBean: void sendLoginInfoToSso(other.domain.ChangeOrderResult)",
-                                        27, "cancel.queue.MsgReveiceBean: void receiveQueueInfo(java.lang.Object)",
-                                        new String[] { "changeOrderResult" },
-                                        new String[] { "changeOrderResult" });
-                }
-                for (RequestWirePoint wp : wps.keySet()) {
-                        if (reqMethod.contains(wp.reqMethod) && lineNum == wp.lineNum) {
-                                // App.p("!!! " + reqMethod + ", " + wps.get(wp));
-                                return wps.get(wp);
-                        }
-                }
-                // return wps.get(new HTTPRequestWirePoint(reqMethod, lineNum));
-                return null;
+        if (wps == null) {
+            wps = new HashMap<>();
+
+            // bannedStmts = new ArrayList<>();
+
+            List<String> strs = Utils.readFrom(App.caseStudyPath + "httpWires");
+            for (int i = 0; i < strs.size() / 5; i++) {
+                // App.p("!!! " + strs.get(i + 1));
+                int start = i * 5;
+                add(strs.get(start), Integer.valueOf(strs.get(start + 1)),
+                        strs.get(start + 2),
+                        strs.get(start + 3).split(","),
+                        strs.get(start + 4).split(","));
+            }
         }
-
-        public static void add(String reqMethod, int lineNum, String recvMethod, String[] reqParams,
-                        String[] recvParams) {
-                RequestWirePoint reqp = new RequestWirePoint(reqMethod, lineNum);
-                List<String> reqarr = Arrays.asList(reqParams);
-
-                List<String> recvarr = Arrays.asList(recvParams);
-                HTTPReceiveWirePoint recvp = new HTTPReceiveWirePoint(recvMethod, reqarr, recvarr);
-                wps.put(reqp, recvp);
+        for (RequestWirePoint wp : wps.keySet()) {
+            if (reqMethod.contains(wp.reqMethod) && lineNum == wp.lineNum) {
+                // App.p("!!! " + reqMethod + ", " + wps.get(wp));
+                return wps.get(wp);
+            }
         }
+        // return wps.get(new HTTPRequestWirePoint(reqMethod, lineNum));
+        return null;
+    }
+
+    public static void add(String reqMethod, int lineNum, String recvMethod, String[] reqParams,
+            String[] recvParams) {
+        RequestWirePoint reqp = new RequestWirePoint(reqMethod, lineNum);
+        List<String> reqarr = Arrays.asList(reqParams);
+
+        List<String> recvarr = Arrays.asList(recvParams);
+        HTTPReceiveWirePoint recvp = new HTTPReceiveWirePoint(recvMethod, reqarr, recvarr);
+        // App.p("AAAA " + reqp + ", " + recvp);
+        wps.put(reqp, recvp);
+    }
 }
